3beedb05c08c9ddcea97f7ff6f77c543afca2955
ClfFu <143682649+clfpwp@users.noreply.github.com>
2025-02-08 14:11:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.api.message_components import At
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
-class MyPlugin(Star):
+@register("ignore_at_all", "Your Name", "一个忽略包含@全体成员消息的插件", "1.0.0", "repo url")
+class IgnoreAtAllPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    @filter.event_message_type(filter.EventMessageType.ALL)
+    async def ignore_at_all(self, event: AstrMessageEvent):
+
+        for comp in event.message_obj.message:
+            if isinstance(comp, At):
+                if str(comp.qq).lower() in {"all", "全体成员"}:
+                    logger.info("检测到为@全体成员，停止事件传播。")
+                    event.stop_event()
+                    return
